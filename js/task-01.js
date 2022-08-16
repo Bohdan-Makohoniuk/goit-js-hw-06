d7453e2a1bdffc437504ec9d3ffafb9a12b2feab
Bohdan <makohoniuk.bo@gmail.com>
2022-08-16 23:42:49 +0300

    start hw js add Priter.json

---
+++ js/task-01.js
@@ -0,0 +1,8 @@
+const ItemsEl = document.querySelectorAll('.item');
+console.log(ItemsEl);
+const liItemEl = ItemsEl.length
+console.log(` Number of categories:${liItemEl}`);
+
+const categoriesEl = document.querySelectorAll('h2');
+
+console.log(categoriesEl);
