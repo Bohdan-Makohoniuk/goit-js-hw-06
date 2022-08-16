269794e0af16641fe6d787bc58c6511515c97812
Bohdan <makohoniuk.bo@gmail.com>
2022-08-17 00:02:32 +0300

    add task

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,12 @@
+// 1. Знайти в document елементи з класом item та повернути довжину масиву
 const ItemsEl = document.querySelectorAll('.item');
 console.log(ItemsEl);
 const liItemEl = ItemsEl.length
 console.log(` Number of categories:${liItemEl}`);
 
-const categoriesEl = document.querySelectorAll('h2');
+// 2.Створити метод який перебире li class="item" та повертає назву h2
+// 2.Створити метод який перебире li class="item" та повертає кількість li 
+const categoriesEl = document.querySelectorAll('#categories');
+console.log(...categoriesEl);
 
-console.log(categoriesEl);
+
