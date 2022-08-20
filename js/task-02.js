f184bf0c01742be527bb5199dbb4538554188cf0
Bohdan <makohoniuk.bo@gmail.com>
2022-08-20 21:50:42 +0300

    task_02

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const list = document.createElement('li');
+list.classList.add(name);
+list.textContent = 'Potatoes';
+document.body.appendChild(list);
+console.log(list);
+
+
+// ingredients.forEach(function (ingredient) {
+//     console.log(`categories: ${value.textContent}`);
+// });
